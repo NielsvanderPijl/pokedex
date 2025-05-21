4a125f1c3088105347d24bbbde592faeceb7a094
NielsvanderPijl <199636@novacollege.nl>
2025-05-21 13:01:25 +0200

    formulier added

--- index.php
+++ index.php
@@ -17,16 +17,31 @@ include "function.php";
 
     <div class="row">
         <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="img/donaldtrump.jpg" class="card-img-top" alt="...">
+
+            <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Donald Trump</h5>
-                    <p class="card-text">De 47ste presidenten van Amerika.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <p class="card-text">De 47ste president van amerika.</p>
                 </div>
+                <img src="img/donaldtrump.jpg" class="card-img-bottom" alt="...">
             </div>
+
+
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+
+
+
         </div>
-        <script src="js/bootstrap.bundle.min.js"></script>
-        <script src="js/main.js"></script>
+    </div>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/main.js"></script>
 </body>
 </html>
