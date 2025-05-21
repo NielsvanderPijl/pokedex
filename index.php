057fe3b4bb6f4ccfb389382ae38b676cb5bb8f72
NielsvanderPijl <199636@novacollege.nl>
2025-05-21 11:55:16 +0200

    donald trump kaart added

--- index.php
+++ index.php
@@ -7,16 +7,26 @@ include "function.php";
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Pokedex - by 1NielsNL1</title>
+    <title>Pokedex - by Niels</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/styles.css" rel="stylesheet">
 </head>
 <body>
+
 <div class="container">
 
-</div>
-
-<script src="js/bootstrap.bundle.min.js"></script>
-<script src="js/main.js"></script>
+    <div class="row">
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="img/donaldtrump.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Donald Trump</h5>
+                    <p class="card-text">De 47ste presidenten van Amerika.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/main.js"></script>
 </body>
 </html>
